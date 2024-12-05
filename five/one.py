24be7f30681077ec096d2443f808d8d35ca4b469
Richard <richard.martin@goteborgenergi.se>
2024-12-05 18:28:17 +0100

    halvway day 5 part 1

--- five/one.py
+++ five/one.py
@@ -4,7 +4,58 @@ import numpy as np
 #%%
 
 with open('testinput.txt') as f:
-    data = f.readlines()
+    data = f.read()
 
 data
+# %%
+"""
+def add_deps(basekey, rule):
+    if not rule.value in all_rules[basekey]: add rule.value to values
+
+    for new_rule in list where new_rule.key equals rule.value:
+        add_deps(basekey, new_rule)
+
+
+Add all rule-keys to dict as keys
+For each rule in rules:
+    add_deps(rule.key, rule)
+
+"""
+#%%
+data = str(data.split('\n')).split("\'\'")
+rules = data[0]
+rules = eval(rules[:-2]+']')
+updates = data[1]
+
+#%%
+rulebook = []
+keys = set()
+for entry in rules:
+    k, v = entry.split('|')
+    rulebook.append({int(k): [int(v)]})
+    keys.add(int(k))
+
+all_rules = {key: [] for key in keys}
+rulebook
+
+#%%
+
+
+def add_deps(basekey, rule):
+    for item in rule.items():
+        for value in item[1]:
+            if not value in all_rules[basekey]:
+                all_rules[basekey].append(value)
+                
+    pass
+
+
+for rule in rulebook:
+    for item in rule.items():
+         for value in item[1]:
+             print(value)
+            # if not value in rulebook[basekey]:
+                # rulebook[basekey].append(value) 
+
+
 # %%
