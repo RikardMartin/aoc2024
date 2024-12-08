a43f6d77ab9d81d05a904eec8a1f6f175082c128
MARTIN Richard <richard.martin2@soprasteria.com>
2024-12-08 21:20:09 +0100

    day 6 part 2

--- six/two.py
+++ six/two.py
@@ -4,9 +4,8 @@ import numpy as np
 #%% load input
 
 input = []
-with open('testinput.txt') as f:
+with open('input.txt') as f:
     for line in f:
-        print(line)
     
         row = []
         for c in line[:-1]:
@@ -14,65 +13,55 @@ with open('testinput.txt') as f:
         input.append(row)
 
 input = np.array(input)
-input = input.T
-
+print(input)
 #%% traverse map
-def outside(pos):
-    if (pos[1][0] < 0) or (pos[0][0] < 0) or (pos[0][0] >= input.shape[0]) or (pos[1][0] >= input.shape[1]):
-        return True
-
-    return False
 
 def take_step(dir, pos):
     match dir:
 
         case '^':
-            step = (pos[0], pos[1] - 1)
-            if outside(step):
+            step = (pos[0]-1, pos[1])
+            if step[0][0] < 0:
                 return True, step, dir
             else:
-                symbol = input[step]
-                if symbol == '#':
-                    step = (pos[0] + 1, pos[1])
+                if input[step] == '#':
+                    step = (pos[0], pos[1])
                     dir = '>'
 
                 return False, step, dir
 
         case '>':
-            step = (pos[0] + 1, pos[1])
-            if outside(step):
+            step = (pos[0], pos[1]+1)
+            if step[1][0] >= input.shape[1]:
                 return True, step, dir
             
             else:
-                symbol = input[step]
-                if symbol == '#':
-                    step = (pos[0], pos[1] + 1)
+                if input[step] == '#':
+                    step = (pos[0], pos[1])
                     dir = 'v'
 
                 return False, step, dir
 
         case 'v':
-            step = (pos[0], pos[1] + 1)
-            if outside(step):
+            step = (pos[0]+1, pos[1])
+            if step[0][0] >= input.shape[0]:
                 return True, step, dir
             
             else:
-                symbol = input[step]
-                if symbol == '#':
-                    step = (pos[0] - 1, pos[1])
+                if input[step] == '#':
+                    step = (pos[0], pos[1])
                     dir = '<'
 
                 return False, step, dir
             
         case '<':
-            step = (pos[0] - 1, pos[1])
-            if outside(step):
+            step = (pos[0], pos[1]-1)
+            if step[1][0] < 0:
                 return True, step, dir
             
             else:
-                symbol = input[step]
-                if symbol == '#':
-                    step = (pos[0], pos[1] - 1)
+                if input[step] == '#':
+                    step = (pos[0], pos[1])
                     dir = '^'
 
                 return False, step, dir
@@ -83,31 +72,50 @@ def take_step(dir, pos):
 
 #%% sätt ut hinder
 loops = []
+start_pos = np.where(input=='^')
+start_dir = '^'
 
-for x in range(input.shape[0]):
-    for y in range(input.shape[1]):
+total_n = input.shape[0]*input.shape[1]
+for x in range(0, input.shape[0]):
+    for y in range(0, input.shape[1]):
+
+        # progress = int(100*(x+1)*(y+1)/total_n)
+        if y % 10 == 0:
+            print("progress", x, y)
+        
         original = input[x,y]
-        input[x,y] = '#'
+        if (original == '.'):
+            input[x,y] = '#'
 
         visited = np.zeros(input.shape, dtype='int')
+        visited[start_pos] = 1
         directions = np.zeros(input.shape, dtype='str')
-        position = np.where(input=='^')
-        dir = '^'
+        directions[start_pos] = start_dir
         out = False
 
-        print(dir, ' : ', position, ' : ', out)
+        position = start_pos
+        dir = start_dir
+        # print(dir, ' : ', position, ' : ', out)
+        i = 0
         while not out:
-            
             out, position, dir = take_step(dir, position)
-            print(dir, ' : ', position, ' : ', out)
+            # print(dir, ' : ', position, ' : ', out)
+            i += 1
             if not out:
                 if (directions[position] == dir) and (visited[position] == 1):
                     loops.append((x,y))
-                    input[x,y] = original
+                    break
+                if i > total_n:
+                    loops.append((x,y))
                     break
                     
                 visited[position] = 1
                 directions[position] = dir
+            
+        input[x,y] = original
 
-
+print(loops)
+print("numer of loops:", len(loops))
+# print(visited.sum())
+# print(visited)
 # %%
