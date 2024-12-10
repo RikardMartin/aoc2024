911f9aa1c250ec37a0543aa13cff2a09a90e56f6
Richard <richard.martin@goteborgenergi.se>
2024-12-10 09:52:45 +0100

    day 7 part 2

---
+++ seven/two.py
@@ -0,0 +1,57 @@
+#%%
+from itertools import permutations, product
+
+#%%
+calibration_result = 0
+
+def mul(test, i):
+    test[i+1] = test[i] * test[i+1]
+    # print(test[i+1], '=', test[i], '*', test[i+1])
+    return test
+
+def add(test, i):
+    test[i+1] = test[i] + test[i+1]
+    # print(test[i+1], '=', test[i], '+', test[i+1])
+    return test
+
+def concat(test, i):
+    # print(test[i+1], '=', test[i], '+', test[i+1])
+    test[i+1] = int( str(test[i]) + str(test[i+1]) )
+    
+    return test
+
+def evaluate(answer, test):
+    repeats = len(test)-1
+    perms = [p for p in product([add, mul, concat], repeat=repeats)]
+    # print(perms)  
+    for perm in perms:
+        # print(perm)
+        result = [n for n in test]
+        for i in range(repeats):
+            result = perm[i](result, i)
+            # print('result:', result[-1])
+        if result[-1]==answer:
+            return True
+        
+with open('input.txt') as f:
+    for line in f:
+        
+        answer, test = line.split(':')
+        answer = int(answer)
+        
+        test = test.strip().split(' ')
+        test = [int(x) for x in test]
+        # print(test)
+
+        result = evaluate(answer, test)
+        print('final:', result, answer)
+        if result:
+            calibration_result += answer
+
+#%%
+print(calibration_result)
+
+
+
+    
+# %%
