9055c39adee886c3a667686cf7767a4822aa2303
Richard <richard.martin@goteborgenergi.se>
2024-12-16 17:54:54 +0100

    day 11 start

---
+++ eleven/one.py
@@ -0,0 +1,18 @@
+#%%
+"""
+def process_stone(stone):
+    go through conditions and apply appropriate rule
+    return array of new stone(s)
+
+def process_array(array):
+    define new array
+    for stone in array:
+        call process_stone() and append to new array
+    return new array
+
+
+array = input    
+for i in range(26): # "blink 25 times"
+    array = process_array(array)
+    print(len(array))
+"""
