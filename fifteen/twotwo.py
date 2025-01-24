2e78977b004d11a53b0e486517d2828dfea3b3a5
Richard <richard.martin@goteborgenergi.se>
2025-01-24 17:55:05 +0100

    day 15 part 2 restart cont

--- fifteen/twotwo.py
+++ fifteen/twotwo.py
@@ -9,41 +9,52 @@ def get_state(position, action):
     pass
 
 def set_pos(pos, action, n_steps):
-    grid[pos] = '.'
+    if action=='^': pos = (pos[0]-n_steps, pos[1])
+    elif action=='>': pos = (pos[0], pos[1]+n_steps)
+    elif action=='v': pos = (pos[0]+n_steps, pos[1])
+    elif action=='<': pos = (pos[0], pos[1]-n_steps)
+    
+    return pos
+
+def move_left(pos):
+    count = 3
+    state = grid[(pos[0], pos[1]-count)]
+    while state in ('[', ']'):
+        count += 1
+        state = grid[(pos[0], pos[1]-count)]
+    if state=='#':
+        return pos
+    else:
+        for i in range(count, 1, -1):
+            grid[(pos[0], pos[1]-i)] = grid[(pos[0], pos[1]-i+1)]
+        grid[(pos[0], pos[1])] = '.'
+
+def move_up(pos):
     pass
-
-
-def take_action(action):
-    symbol = get_state(position, action)
-    while not symbol=='#':
-        if symbol=='.':
-            position = set_pos(position, action, 1)
-            break
-
-        else:
-            if action=='<':
-                pass
-            if action=='>':
-                pass
-            if action=='^':
-                pass
-            if action=='v':
-                pass
-            
-    return position
-
-
-
-
-
-
-
+        
 #%%
 grid = get_start_grid(input)
 position = get_position()
 
 for action in actions:
-    position = take_action(action)
+    symbol = get_state(position, action)
+    while not symbol=='#':
+
+        if symbol=='.':
+            grid[position] = '.'
+            position = set_pos(position, action, 1)
+            grid[position] = '@'
+            break
+
+        else:
+            if (action=='<'):
+                position = move_left(position)
+            if action=='>':
+                pass
+            if action=='^':
+                position = move_up(position)
+            if action=='v':
+                pass
 
 gps_sum = compute_gps_sum(grid)
 
